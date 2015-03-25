327cd22d3f98c403ac07962ffee6f7f17dbae1b0
RaySuhyunLee <raysuhyunlee@gmail.com>
2015-03-25 19:20:12 +0900

    I did nothing

--- warm_ray/main.c
+++ warm_ray/main.c
@@ -3,3 +3,16 @@
 int main(void) {
     printf("Hello, World!");
 }
+
+int init_module() {
+    int ret_val;
+    ret_val = register_chrdev(MAJOR_NUM, DEVICE_NAME, &Fops);
+
+    if (ret_val < 0) {
+        printk(KERN_ALERT "%s failed with %d\n",
+                "Sorry, registering the character device ", ret_val);
+        return ret_val;
+    }
+}
+
+
