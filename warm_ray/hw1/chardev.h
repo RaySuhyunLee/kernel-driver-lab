a5398b44a7197ed8825d791ff05a70e225f225cf
RaySuhyunLee <raysuhyunlee@gmail.com>
2015-04-11 01:45:46 +0900

    IOCTL command for counting ancestors

--- warm_ray/hw1/chardev.h
+++ warm_ray/hw1/chardev.h
@@ -10,6 +10,7 @@ struct task_info {
 
 #define MAJOR_NUM 100
 #define IOCTL_GET_TASK_INFO _IOR(MAJOR_NUM, 1, struct task_info* )
+#define IOCTL_GET_TASK_COUNT _IOR(MAJOR_NUM, 2, int* count)
 
 #define DEVICE_FILE_NAME "char_dev"
 
